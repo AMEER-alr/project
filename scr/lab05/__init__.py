2f165fbc8648503340ada2db0c4b71148895299c
AMEER-alr <k4424441@gmail.com>
2025-11-06 11:11:14 +0300

    all

--- scr/lab05/__init__.py
+++ scr/lab05/__init__.py
@@ -1,1 +1,4 @@
-# This file makes the lab05 directory a Python package
+from .json_to_csv import json_to_csv, csv_to_json
+from .csv_to_xlsx import csv_to_xlsx
+
+__all__ = ['json_to_csv', 'csv_to_json', 'csv_to_xlsx']
